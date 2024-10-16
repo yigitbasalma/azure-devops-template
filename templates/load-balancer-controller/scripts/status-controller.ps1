9eb0bb652fc38fc34b1f99f5e7ad85bb96bec042
Yiğit Can Başalma <yigit.basalma@local>
2024-10-16 13:33:06 +0300

    Fix

--- templates/load-balancer-controller/scripts/status-controller.ps1
+++ templates/load-balancer-controller/scripts/status-controller.ps1
@@ -24,7 +24,7 @@ switch ($Operation) {
             } | ConvertTo-Json
 
             # Send the request to disable the vServer
-            $response = Invoke-RestMethod -Uri "$baseApiUrl/vserver?action=disable" -Method Put -Headers @{
+            $response = Invoke-RestMethod -Uri "$baseApiUrl/vserver?action=disable" -Method Post -Headers @{
                 "Content-Type" = "application/json"
                 "Authorization" = "Basic $authInfo"
             } -Body $body
