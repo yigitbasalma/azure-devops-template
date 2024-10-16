1545ce71c114b471c7ce9ca3aa8935657388b9ad
Yiğit Can Başalma <yigit.basalma@local>
2024-10-16 12:32:18 +0300

    Fix

--- templates/load-balancer-controller/scripts/status-controller.ps1
+++ templates/load-balancer-controller/scripts/status-controller.ps1
@@ -12,8 +12,28 @@ switch ($Operation) {
         # Enable vservers
     }
     "disable" {
+        $authInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes("$LBUsername`:$LBPassword"))
+
         $($VServers | ConvertFrom-Json) | ForEach-Object {
-            Write-Host "$($_.Name) server is disabled."
+            # Define the body of the request to disable the vServer
+            $body = @{
+                lbvserver = @{
+                    name = $_.Name
+                }
+            } | ConvertTo-Json
+
+            # Send the request to disable the vServer
+            $response = Invoke-RestMethod -Uri "$(LBAddress)/lbvserver?action=disable" -Method Put -Headers @{
+                "Content-Type" = "application/json"
+                "Authorization" = "Basic $authInfo"
+            } -Body $body -SkipCertificateCheck
+
+            # Check the response
+            if ($response.errorcode -eq 0) {
+                Write-Host "$($_.Name) server is disabled."
+            } else {
+                Write-Host "Failed to disable vServer '$($_.Name)'. Error: $($response.message)"
+            }
         }
     }
     default {
