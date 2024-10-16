c8b0dff96e3133261f7ef415c63b8d25f77af07c
Yiğit Can Başalma <yigit.basalma@local>
2024-10-16 20:08:31 +0300

    Fix

--- templates/load-balancer-controller/scripts/status-controller.ps1
+++ templates/load-balancer-controller/scripts/status-controller.ps1
@@ -18,29 +18,27 @@ switch ($Operation) {
     "disable" {
         $($VServers | ConvertFrom-Json) | ForEach-Object {
             if ( $DeploymentPart -ne $_.DeployGroup ) {
-                continue
-            }
+                # Define the body of the request to disable the vServer
+                $body = @{
+                    servicegroup = @{
+                        servername = $_.Name
+                        servicegroupname = $_.ServiceGroup
+                        port = $_.Port
+                    }
+                } | ConvertTo-Json
 
-            # Define the body of the request to disable the vServer
-            $body = @{
-                servicegroup = @{
-                    servername       = $_.Name
-                    servicegroupname = $_.ServiceGroup
-                    port             = $_.Port
+                # Send the request to disable the vServer
+                $response = Invoke-RestMethod -Uri "$baseApiUrl/servicegroup?action=disable" -Method Post -Headers @{
+                    "Content-Type" = "application/json"
+                    "Authorization" = "Basic $authInfo"
+                } -Body $body
+
+                # Check the response
+                if ($response.message -eq $null) {
+                    Write-Host "$( $_.Name ) server is disabled."
+                } else {
+                    Write-Host "Failed to disable vServer '$( $_.Name )'. Error: $( $response.message )"
                 }
-            } | ConvertTo-Json
-
-            # Send the request to disable the vServer
-            $response = Invoke-RestMethod -Uri "$baseApiUrl/servicegroup?action=disable" -Method Post -Headers @{
-                "Content-Type" = "application/json"
-                "Authorization" = "Basic $authInfo"
-            } -Body $body
-
-            # Check the response
-            if ($response.message -eq $null) {
-                Write-Host "$($_.Name) server is disabled."
-            } else {
-                Write-Host "Failed to disable vServer '$($_.Name)'. Error: $($response.message)"
             }
         }
     }
