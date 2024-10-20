5d4bef43e3356a5c9739b30d49f886701d5bf8c6
Yiğit Can Başalma <yigit.basalma@local>
2024-10-20 20:08:36 +0300

    Fix

--- templates/deployment-controller/scripts/pre-checks.ps1
+++ templates/deployment-controller/scripts/pre-checks.ps1
@@ -1,25 +1,15 @@
 param (
     [string]$Environment,
     [string]$Packages,
-    [string]$CurrentBuildPath,
+    [string]$AgentBuildPath,
     [string]$BuildNumber
 )
 
-# ANSI escape codes for colors
-$green = "`e[32m"
-$yellow = "`e[33m"
-$red = "`e[31m"
-$reset = "`e[0m"
-
 foreach ( $package in $($Packages | ConvertFrom-Json) ) {
-    Write-Host "------------Starting to the artifact existence pre-check---------------"
-
-    $ArtifactPath = "$CurrentBuildPath\$BuildNumber\$($package.artifact.name)"
+    $ArtifactPath = "$AgentBuildPath\$BuildNumber\$($package.artifact.name)"
     if ( -NOT (Test-Path $ArtifactPath) ) {
-        Write-Host "$red Package not found for $($package.name) on $ArtifactPath at $env:COMPUTERNAME $reset"
+        Write-Host "Package not found for $($package.name) on $ArtifactPath at $env:COMPUTERNAME"
         continue
     }
-    Write-Host "$green Package found for $($package.name) on $ArtifactPath at $env:COMPUTERNAME $reset"
-
-    Write-Host "------------End to the artifact existence pre-check---------------"
+    Write-Host "Package found for $($package.name) on $ArtifactPath at $env:COMPUTERNAME"
 }
