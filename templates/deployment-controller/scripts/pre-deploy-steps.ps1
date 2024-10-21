70cc7378f72383bc5a3afb315f59c25b6df725d9
Yiğit Can Başalma <yigit.basalma@local>
2024-10-21 23:36:29 +0300

    Fix

--- templates/deployment-controller/scripts/pre-deploy-steps.ps1
+++ templates/deployment-controller/scripts/pre-deploy-steps.ps1
@@ -5,6 +5,54 @@ param (
     [string]$BuildNumber
 )
 
+function Take-Backup {
+    param (
+        [string]$SiteName,
+        [string]$BackupRootPath
+    )
+
+    $tempDir = "C:\BackupTemp"
+    $backupPath = "$BackupRootPath\$SiteName"
+    $backupName = "$(Get-Date -Format 'yyyyMMdd_HHmmss').zip"
+    $iisWebsitePath = Get-WebFilePath "IIS:\Sites\$SiteName"
+
+    if ( Test-Path $tempDir ) {
+        Remove-Item -Path $tempDir -Recurse -Force
+    } else {
+        New-Item -ItemType "directory" -Path $tempDir
+    }
+
+    if ( -NOT(Test-Path $BackupPath) ) {
+        New-Item -ItemType "directory" -Path $BackupPath
+    }
+
+    & robocopy $iisWebsitePath.FullName $tempDir /MIR /XF UmbracoTraceLog* /XD *cache* /NFL /NDL /NJH /NJS
+
+    Add-Type -AssemblyName System.IO.Compression.FileSystem
+    [System.IO.Compression.ZipFile]::CreateFromDirectory($tempDir,"$backupPath\$backupName")
+
+    Remove-Item -Path $tempDir -Recurse -Force
+
+    Write-Host "[$backupName] Backup procedure completed for the $SiteName."
+}
+
+function Do-Retention-Policy {
+    param (
+        [string]$SiteName,
+        [string]$BackupRootPath,
+        [integer]$Retention
+    )
+
+    $backupPath = "$BackupRootPath\$SiteName"
+    $objects = Get-ChildItem -Path $backupPath | Sort-Object -Descending -Property LastWriteTime | select -Skip $Retention
+
+    $objects | ForEach-Object {
+        $oldBackup = "$BackupRootPath\$($_.Name)"
+        Remove-Item -Path $oldBackup -Recurse -Force
+        Write-Host "[$oldBackup] Old backup file removed."
+    }
+}
+
 foreach ( $package in $($Packages | ConvertFrom-Json) ) {
     if ( (Get-WebAppPoolState -Name "$($package.iis.poolName)").Value -eq "Stopped" ) {
         Write-Host "[$($package.iis.poolName)] The application pool is already stopped."
@@ -26,6 +74,14 @@ foreach ( $package in $($Packages | ConvertFrom-Json) ) {
             Start-Sleep -Seconds 30
         }
         Write-Host "[$($package.iis.name)] The website is now stopped."
+    }
+
+    if ( $package.backup.enabled ) {
+        Take-Backup -SiteName $package.iis.name -BackupRootPath $package.backup.path
+
+        if ( $package.backup.retention -gt 0 ) {
+            Do-Retention-Policy -SiteName $package.iis.name -BackupRootPath $package.backup.path -Retention $package.backup.retention
+        }
     }
 }
 
