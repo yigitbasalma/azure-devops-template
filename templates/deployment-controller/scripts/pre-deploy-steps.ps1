15ca82093af6ebcb05d49a0d126d1326cab735d3
Yiğit Can Başalma <yigit.basalma@local>
2024-10-21 18:45:51 +0300

    Fix

--- templates/deployment-controller/scripts/pre-deploy-steps.ps1
+++ templates/deployment-controller/scripts/pre-deploy-steps.ps1
@@ -6,12 +6,16 @@ param (
 )
 
 foreach ( $package in $($Packages | ConvertFrom-Json) ) {
-    Stop-WebAppPool -Name "$($package.iis.poolName)"
-    while ( (Get-WebAppPoolState -Name "$($package.iis.poolName)").Value -ne "Stopped" ) {
-        Write-Host "[$($package.iis.poolName)] Waiting for the application pool to stop..."
-        Start-Sleep -Seconds 1
+    if ( (Get-WebAppPoolState -Name "$($package.iis.poolName)").Value -ne "Stopped" ) {
+        Write-Host "[$($package.iis.poolName)] The application pool is already stopped."
+    } else {
+        Stop-WebAppPool -Name "$($package.iis.poolName)"
+        while ( (Get-WebAppPoolState -Name "$($package.iis.poolName)").Value -ne "Stopped" ) {
+            Write-Host "[$($package.iis.poolName)] Waiting for the application pool to stop..."
+            Start-Sleep -Seconds 1
+        }
+        Write-Host "[$($package.iis.poolName)] The application pool is now stopped."
     }
-    Write-Host "[$($package.iis.poolName)] The application pool is now stopped."
 }
 
 exit 0
