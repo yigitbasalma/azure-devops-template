0790650b364ea36d473a3e210823671a756e1421
Yiğit Can Başalma <yigit.basalma@local>
2024-10-21 22:13:27 +0300

    Fix

--- templates/deployment-controller/scripts/pre-deploy-steps.ps1
+++ templates/deployment-controller/scripts/pre-deploy-steps.ps1
@@ -12,7 +12,7 @@ foreach ( $package in $($Packages | ConvertFrom-Json) ) {
         Stop-WebAppPool -Name "$($package.iis.poolName)"
         while ( (Get-WebAppPoolState -Name "$($package.iis.poolName)").Value -ne "Stopped" ) {
             Write-Host "[$($package.iis.poolName)] Waiting for the application pool to stop..."
-            Start-Sleep -Seconds 1
+            Start-Sleep -Seconds 30
         }
         Write-Host "[$($package.iis.poolName)] The application pool is now stopped."
     }
@@ -23,7 +23,7 @@ foreach ( $package in $($Packages | ConvertFrom-Json) ) {
         Stop-WebSite -Name "$($package.iis.name)"
         while ( (Get-WebSite -Name "$($package.iis.name)").Value -ne "Stopped" ) {
             Write-Host "[$($package.iis.name)] Waiting for the website to stop..."
-            Start-Sleep -Seconds 1
+            Start-Sleep -Seconds 30
         }
         Write-Host "[$($package.iis.name)] The website is now stopped."
     }
