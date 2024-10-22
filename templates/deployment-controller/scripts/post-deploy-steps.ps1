709583bea443c1535ec4747cbf23c689cbb7e7d7
Yiğit Can Başalma <yigit.basalma@local>
2024-10-22 22:16:12 +0300

    Fix

--- templates/deployment-controller/scripts/post-deploy-steps.ps1
+++ templates/deployment-controller/scripts/post-deploy-steps.ps1
@@ -48,6 +48,28 @@ function Do-Healthcheck {
 }
 
 foreach ( $package in $($Packages | ConvertFrom-Json) ) {
+    if ( (Get-WebAppPoolState -Name "$($package.iis.poolName)").Value -eq "Started" ) {
+        Write-Host "[$($package.iis.poolName)] The application pool is already started."
+    } else {
+        Start-WebAppPool -Name "$($package.iis.poolName)"
+        while ( (Get-WebAppPoolState -Name "$($package.iis.poolName)").Value -ne "Started" ) {
+            Write-Host "[$($package.iis.poolName)] Waiting for the application pool to start..."
+            Start-Sleep -Seconds 30
+        }
+        Write-Host "[$($package.iis.poolName)] The application pool is now started."
+    }
+
+    if ( (Get-WebSite -Name "$($package.iis.name)").State -eq "Started" ) {
+        Write-Host "[$($package.iis.name)] The website is already started."
+    } else {
+        Start-WebSite -Name "$($package.iis.name)"
+        while ( (Get-WebSite -Name "$($package.iis.name)").State -ne "Started" ) {
+            Write-Host "[$($package.iis.name)] Waiting for the website to start..."
+            Start-Sleep -Seconds 30
+        }
+        Write-Host "[$($package.iis.name)] The website is now started."
+    }
+
     if ( $package.healthcheck.enabled ) {
         Do-Healthcheck -AppAddr $package.iis.host -Path $package.healthcheck.path -ReturnCodes $package.healthcheck.returnCodes -ExpectedString $package.healthcheck.expectedString
     }
